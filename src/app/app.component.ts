a6cd531a34818233fdc6130f80a6ef07219304a9
PakkiBL <lincymaryn@gmail.com>
2025-02-28 21:42:36 +0530

    New_changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,33 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   // imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['app.component.css']
 })
 export class AppComponent {
   title = 'Tourism';
+
+  ngOnInit(): void {
+    document.addEventListener("DOMContentLoaded", () => {
+      const navLinks = document.querySelectorAll("nav ul li a");
+
+      window.addEventListener("scroll", () => {
+          let fromTop = window.scrollY;
+
+          navLinks.forEach(link => {
+              const section = document.querySelector(link.getAttribute("href")!);
+
+              if (section) {
+                  const offsetTop = (section as HTMLElement).offsetTop - 50; // Adjust for navbar height
+                  const offsetBottom = offsetTop + section.clientHeight;
+
+                  if (fromTop >= offsetTop && fromTop < offsetBottom) {
+                      link.classList.add("active");
+                  } else {
+                      link.classList.remove("active");
+                  }
+              }
+          });
+      });
+    });
+  }
 }
